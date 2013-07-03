0b6006351d1f072f319dc02552cbb99798aace58
dafeng <dafeng.xdf@taobao.com>
2013-07-03 13:24:50 +0800

    add frame

--- 1.0/build/plugin/loader-min.js
+++ 1.0/build/plugin/loader-min.js
@@ -1,3 +1,3 @@
-/*! autoResponsive - v1.0 - 2013-07-03 1:00:05 PM
+/*! autoResponsive - v1.0 - 2013-07-03 1:24:48 PM
 * Copyright (c) 2013 xudafeng; Licensed  */
 KISSY.add("gallery/autoResponsive/1.0/plugin/loader",function(e){"use strict";function t(){}return e.Event,e.augment(t,{init:function(){}}),t},{requires:["event"]});
